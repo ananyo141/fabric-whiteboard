55d338978367e9c4eab2acbb4609b57b71795bd3
Ananyobrata Pal <ananyo141@gmail.com>
2023-05-18 18:51:23 +0530

    Add functionality feat:  - add download, clear, erase functionality  - give app title  - center align fix:  - use react compatible color strings (in #rrggbb format) chore:  - return if fabricCanvas not present  - set canvasbg in a ref value

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React from "react";
 import Board from "./components/Board";
 
 function App() {
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,15 +5,17 @@ type Props = {};
 
 const Board = (props: Props) => {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const canvasBg = React.useRef<string>("#F9F0F0");
   const [penWidth, setPenWidth] = React.useState(3);
-  const [penColor, setPenColor] = React.useState("#000");
+  const [penColor, setPenColor] = React.useState("#000000");
   const [fabricCanvas, setFabricCanvas] = React.useState<fabric.Canvas>();
+  const [eraserMode, setEraserMode] = React.useState(false);
 
   React.useEffect(() => {
     const canvas = new fabric.Canvas(canvasRef.current as HTMLCanvasElement, {
       width: 1280,
       height: 500,
-      backgroundColor: "#999",
+      backgroundColor: canvasBg.current,
       isDrawingMode: true,
     });
     setFabricCanvas(canvas);
@@ -24,23 +26,47 @@ const Board = (props: Props) => {
   }, [canvasRef]);
 
   const changePenWidth = (width: number) => {
-    if (fabricCanvas) {
-      fabricCanvas.freeDrawingBrush.width = width;
-      setPenWidth(width);
-      fabricCanvas.renderAll.bind(fabricCanvas);
-    }
+    if (!fabricCanvas) return;
+    fabricCanvas.freeDrawingBrush.width = width;
+    setPenWidth(width);
+    fabricCanvas.renderAll.bind(fabricCanvas);
   };
 
   const changePenColor = (color: string) => {
-    if (fabricCanvas) {
-      fabricCanvas.freeDrawingBrush.color = color;
-      setPenColor(color);
-      fabricCanvas.renderAll.bind(fabricCanvas);
-    }
+    if (!fabricCanvas) return;
+    fabricCanvas.freeDrawingBrush.color = color;
+    setPenColor(color);
+    fabricCanvas.renderAll.bind(fabricCanvas);
+  };
+
+  const downloadBoard = () => {
+    if (!fabricCanvas) return;
+
+    const pngData = fabricCanvas.toDataURL({
+      format: "png",
+      quality: 0.8,
+      multiplier: 4,
+    });
+    const link = document.createElement("a");
+    link.download = `fabric-board-${new Date().getTime()}.png`;
+    link.href = pngData;
+    link.click();
+  };
+
+  const clearBoard = () => {
+    if (!fabricCanvas) return;
+    fabricCanvas.clear();
+    fabricCanvas.backgroundColor = canvasBg.current;
+  };
+
+  const toggleEraser = () => {
+    if (!fabricCanvas) return;
+    changePenColor(eraserMode ? "#000000" : canvasBg.current);
+    setEraserMode(!eraserMode);
   };
 
   return (
-    <div className="container w-full h-full">
+    <div className="flex flex-col items-center gap-3 mt-3 w-full h-full">
       <p className="text-3xl">Whiteboard</p>
       <canvas ref={canvasRef}></canvas>
       <div>
@@ -61,6 +87,31 @@ const Board = (props: Props) => {
             value={penColor}
             onChange={(e) => changePenColor(e.target.value)}
           />
+        </div>
+        <div className="flex gap-2 mt-2">
+          <button
+            type="button"
+            className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+            onClick={downloadBoard}
+          >
+            Download
+          </button>
+
+          <button
+            type="button"
+            className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+            onClick={clearBoard}
+          >
+            Clear
+          </button>
+
+          <button
+            type="button"
+            className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+            onClick={toggleEraser}
+          >
+            {eraserMode ? "Erasing Mode" : "Drawing Mode"}
+          </button>
         </div>
       </div>
     </div>
